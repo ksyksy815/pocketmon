5a4b3a06b4a582ca1e6c67489cba584d6aeca5a7
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-05 14:58:17 +0900

    [Update] Updated data receiving logic to show data related to poketmon on the list

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
+export default function PokemonList( { pokemon, pokemonInfo } ) {
 
-export default function PokemonList( { pokemon } ) {
   return (
     <div>
       {pokemon.map( pokemon => (
-        <div key={pokemon}>{pokemon}</div>
+        <div key={pokemon.name}>
+          <span>{pokemon.name}</span>
+          <section>
+            <div><img src={pokemonInfo.image} alt={pokemon.name}></img></div>
+            <div className="abilities">
+              {pokemonInfo.abilities.reduce((acc,cur,i,arr) => i !== arr.length-1 ? acc+`${cur}, ` : acc+`${cur}`, "")}
+            </div>
+            <div className="stats">
+              {pokemonInfo.stats}
+            </div>
+          </section>
+        </div>
       ))}
     </div>
   )
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,10 +1,80 @@
 import { useState, useEffect} from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 import PokemonList from '../components/PokemonList'
 import Pagination from '../Pagination'
 
+const ListWrapper = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  ul {
+    width: 80%;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+  }
+  .pokemonLi {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #252525;
+  }
+
+  .pokemonInfo {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    .pokemonImage {
+      width: 30%;
+      background: yellow;
+    }
+
+    .pokemonDetails {
+      flex: 1 1 70%;
+      display: flex;
+      flex-direction: column;
+    }
+  }
+
+  .abilities {
+    display: flex;
+    column-gap: 1rem;
+    .ability {
+      background: var(--sky-blue-color);
+      padding: 0.1rem 0.5rem;
+      border-radius: 10px;
+    }
+  }
+
+  .stats {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+const initialState = [
+  {
+    name: "bulbasaur",
+    url: null
+  },
+  {
+    name: "charmander",
+    url: null
+  }
+]
+
 export default function List() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
+  const [pokemon, setPokemon] = useState(initialState)
+  const [pokemonInfo, setPokemonInfo] = useState([])
   const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=5")
   const [nextPageUrl, setNextPageUrl] = useState()
   const [prevPageUrl, setPrevPageUrl] = useState()
@@ -16,15 +86,38 @@ export default function List() {
     axios.get(currentPageUrl, {
       cancelToken: new axios.CancelToken( c => cancel = c)
     }).then( res => {
-      setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
-      setPokemon(res.data.results.map( poketmon => poketmon.name))
+      setPokemon(res.data.results.map( poketmon => {
+        return {
+          name: poketmon.name,
+          url: poketmon.url
+        }
+      }))
     })
 
     return () => cancel()
 
   }, [currentPageUrl])
+
+  useEffect(() => {
+    pokemon.forEach(el => {
+      axios.get(el.url)
+      .then(res => {
+        setPokemonInfo(prev => {
+          let one = {
+            name: res.data.name,
+            image: res.data.sprites.front_default,
+            abilities: res.data.abilities.reduce((acc, cur) => [...acc, cur.ability.name], []),
+            stats: res.data.stats.reduce((acc, cur) => [...acc, `${cur.stat.name}: ${cur.base_stat}`], []),
+          }
+          return [...prev, one]
+        })
+        setLoading(false)
+      })
+    })
+    
+  }, [pokemon])
 
   function gotoNextPage() {
     setCurrentPageUrl(nextPageUrl)
@@ -37,13 +130,41 @@ export default function List() {
   if (loading) return "Loading..."
 
   return (
-    <div>
-      This is the pocketmon list page.
-      <PokemonList pokemon={pokemon}/>
+    <ListWrapper>
+      <ul>
+        {pokemon.map( pokemon => (
+          <li className="pokemonLi" key={pokemon.name}>
+            <h3>Name: {pokemon.name}</h3>
+            <section className="pokemonInfo">
+              <div className="pokemonImage">
+                <img src={pokemonInfo.image} alt={pokemon.name}></img>
+              </div>
+              <div className="pokemonDetails">
+                <div className="abilities">
+                {
+                  pokemonInfo.filter(el => el.name === pokemon.name)
+                  .map(el=> el.abilities.map(ability => <span className="ability">{ability}</span>))
+                }
+                </div>
+                <div className="stats">
+                  {pokemonInfo.stats}
+                  {
+                  pokemonInfo.filter(el => el.name === pokemon.name)
+                  .map(el=> el.stats.map(stat => <span className="stat">{stat}</span>))
+                }
+                </div>
+              </div>
+            </section>
+          </li>
+        ))}
+      </ul>
       <Pagination
         gotoNextPage={nextPageUrl ? gotoNextPage : null}
         gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
       />
-    </div>
+    </ListWrapper>
   )
 }
+
+
+//
