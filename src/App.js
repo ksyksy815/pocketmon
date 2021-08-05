08897d4ad4ef77a3db4dd04edc368f379c08f776
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-05 12:29:56 +0900

    [Update] Added basic routing, navigation and pages to the main app component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css';
-import PokemonList from './components/PokemonList'
-import Pagination from './Pagination';
-
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { GlobalStyle } from './GlobalStyle.style'
+import Navbar from './components/Navbar';
+import Main from './pages/Main'
+import List from './pages/List';
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
-  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=5")
-  const [nextPageUrl, setNextPageUrl] = useState()
-  const [prevPageUrl, setPrevPageUrl] = useState()
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setLoading(true)
-    let cancel;
-    axios.get(currentPageUrl, {
-      cancelToken: new axios.CancelToken( c => cancel = c)
-    }).then( res => {
-      setLoading(false)
-      setNextPageUrl(res.data.next)
-      setPrevPageUrl(res.data.previous)
-      setPokemon(res.data.results.map( poketmon => poketmon.name))
-    })
-
-    return () => cancel()
-
-  }, [currentPageUrl])
-
-  function gotoNextPage() {
-    setCurrentPageUrl(nextPageUrl)
-  }
-
-  function gotoPrevPage() {
-    setCurrentPageUrl(prevPageUrl)
-  }
-
-  if (loading) return "Loading..."
-
   return (
-    <>
-      <PokemonList pokemon={pokemon}/>
-      <Pagination
-        gotoNextPage={nextPageUrl ? gotoNextPage : null}
-        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
-      />
-    </>
+    <Router>
+      <GlobalStyle/>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+        <Route path='/list'>
+          <List />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
