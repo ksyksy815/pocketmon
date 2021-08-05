d6ed3ee6180cf0abd3b6668307b42c7158eba1cc
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-05 14:58:42 +0900

    [Update] Added the main app components' basic css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import styled from 'styled-components'
 import { GlobalStyle } from './GlobalStyle.style'
 import Navbar from './components/Navbar';
 import Main from './pages/Main'
 import List from './pages/List';
 
+const AppWrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`
+
 function App() {
   return (
     <Router>
       <GlobalStyle/>
-      <Navbar />
-      <Switch>
-        <Route exact path='/'>
-          <Main />
-        </Route>
-        <Route path='/list'>
-          <List />
-        </Route>
-      </Switch>
+      <AppWrapper>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <Route path='/list'>
+            <List />
+          </Route>
+        </Switch>
+      </AppWrapper>
     </Router>
   );
 }
