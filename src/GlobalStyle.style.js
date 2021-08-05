a3767409d5c175b47c3f3beb4a3758ffb1f446c9
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-05 12:28:57 +0900

    [Update] Deleted the default CSS file and added the global style file

---
+++ src/GlobalStyle.style.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --yellow-color: rgb(251,215,67);
+    --red-color: rgb(254,31,31);
+    --sky-blue-color: rgb(92,185,255);
+    --dark-blue-color: rgb(54,59,129);
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
