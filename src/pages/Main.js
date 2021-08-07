58ec6dab492ee4728b17b26e6c22951cc82c840c
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-07 17:25:36 +0900

    [Update] Added the main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
+import styled from 'styled-components'
+
+const MainWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`
 
 export default function Main() {
+  const [highlight, setHighlight] = useState('squirtle')
+
   return (
-    <div>
-      This is the main page.
-    </div>
+    <MainWrapper>
+      <section>
+        <h1>This week's highlight: {highlight}</h1>
+      </section>
+    </MainWrapper>
   )
 }
