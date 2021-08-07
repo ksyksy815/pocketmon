609af9a6ff37595ae6617241299a191be284ed3d
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-07 17:21:13 +0900

    [Update] Applied CSS to the pagination buttons

--- src/Pagination.js
+++ src/Pagination.js
@@ -2,7 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 const PaginationWrapper = styled.div`
-  border: 1px solid red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 1rem;
+
+  button {
+    width: 100px;
+    height: 35px;
+    border-radius: 999px;
+    border: none;
+    background-color: var(--dark-blue-color);
+    color: #fff;
+    letter-spacing: 1px;
+    transition: 0.1s;
+
+    &:hover {
+      cursor: pointer;
+      background-color: var(--yellow-color);
+      transform: translateY(-2px);
+    }
+  }
 `
 
 export default function Pagination( {gotoNextPage, gotoPrevPage} ) {
